8c5cd653f79add30c8648d48298aace4fea7ed23
Psalmyhub <90870375+Psalmyhub@users.noreply.github.com>
2026-09-18 20:00:00 +0100

    chore: ignore generated GenLayer template files in eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,13 +5,16 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
+    "genlayer-template-test/.venv/**",
+    "genlayer-template-test/node_modules/**",
+    "genlayer-template-test/.pytest_cache/**",
+    "genlayer-template-test/__pycache__/**",
+    "genlayer-template-test/**/dist/**",
   ]),
 ]);
 
